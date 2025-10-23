f4e06a5f0b4c76c969540e13ab168e33fa33d505
Ilia Gogotchuri <ilia.gogotchuri0@gmail.com>
2025-10-23 16:07:12 +0400

    windows test fix; separate function for version logging
    Signed-off-by: Ilia Gogotchuri <ilia.gogotchuri0@gmail.com>

--- downloader_nightly_test.go
+++ downloader_nightly_test.go
@@ -4,11 +4,39 @@
 package tofudl_test
 
 import (
+	"bytes"
 	"context"
+	"os"
+	"os/exec"
+	"path"
+	"runtime"
 	"testing"
 
 	"github.com/opentofu/tofudl"
+	"github.com/opentofu/tofudl/branding"
 )
+
+// Given a tofu binary, calls "version" subcommand and logs the output
+// This also calls runtime.GC() to cleanup any file handle still held after the test is done (THANKS WINDOWS!)
+func logTofuVersion(t *testing.T, binary []byte) {
+	t.Helper()
+	tmp := t.TempDir()
+	fileName := branding.PlatformBinaryName
+	fullPath := path.Join(tmp, fileName)
+	if err := os.WriteFile(fullPath, binary, 0755); err != nil { //nolint:gosec //We want the binary to be executable.
+		t.Fatal(err)
+	}
+	stdout := bytes.Buffer{}
+
+	cmd := exec.Command(fullPath, "version")
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stdout
+	if err := cmd.Run(); err != nil {
+		t.Fatal(err)
+	}
+	t.Log(stdout.String())
+	runtime.GC()
+}
 
 // Default options, no options passed in
 func TestNightlyDownload(t *testing.T) {
@@ -29,6 +57,8 @@ func TestNightlyDownload(t *testing.T) {
 	if len(binary) == 0 {
 		t.Fatal("Downloaded binary is empty")
 	}
+
+	logTofuVersion(t, binary)
 }
 
 // TestNightlyDownloadWithOptions tests downloading with specific platform/architecture
@@ -56,4 +86,6 @@ func TestNightlyDownloadWithHostOptions(t *testing.T) {
 	if len(binary) == 0 {
 		t.Fatal("Downloaded binary is empty")
 	}
+
+	logTofuVersion(t, binary)
 }
